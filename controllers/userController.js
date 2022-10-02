787970fe5831216a81c73fd11690d45288245a5a
davi38 <davi4alves@gmail.com>
2022-10-02 17:51:53 -0300

    feat: bcrypt installed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const models = require("../models");
+const bcrypt = require("bcryptjs");
 
 const userDB = models.userModel;
 
@@ -33,16 +34,39 @@ const signup = async (req, res, next) => {
   }
 
   let user;
+  // encrypt password with Bcrypt
+  const hashedPassword = bcrypt.hashSync(password);
   try {
-   user= await userDB.create({
-        name,
-        email,
-        password
+    user = await userDB.create({
+      name,
+      email,
+      password: hashedPassword,
     });
   } catch (error) {
-    res.status(500).json({message: "Couldn't save data to database"})
+    res.status(500).json({ message: "Couldn't save data to database" });
   }
   return res.status(200).json(user);
 };
 
-module.exports = { getUser, signup };
+// Login USER
+// POST /api/user/login
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await userDB.findOne({ email });
+  } catch (error) {
+    console.log(error);
+  }
+  if (!existingUser) {
+    return res.status(400).json({ message: "Couldnt find user by this email" });
+  }
+  //compare hashed password
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Incorrect Password" });
+  }
+  return res.status(200).json({ message: "Login Successfull" });
+};
+
+module.exports = { getUser, signup, login };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,7 @@ const controller = require("../controllers");
 
 router.route("/").get(controller.getUser);
 router.route("/signup").post(controller.signup)
+router.route("/login").post(controller.login)
+
 
 module.exports = router;
